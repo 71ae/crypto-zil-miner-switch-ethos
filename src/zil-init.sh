5b8a4927e8f7b6364b218423b2431061ba8a48ea
Andreas E. (71ae) <47578577+71ae@users.noreply.github.com>
2019-08-06 22:33:44 +0200

    Logging (#9)

--- src/zil-init.sh
+++ src/zil-init.sh
@@ -6,27 +6,34 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/g
 
 ZILLOOP="zil-loop"
 
-case "$-" in
-  *i*)
-      INTERACTIVE=1
-      ;;
-  *)
-      INTERACTIVE=0
-      ;;
-esac
+LOG_FILE="/tmp/zil-switch.log"
+
+# Clear Log
+echo > $LOG_FILE
+
+# Define Log Function
+Log() {
+  msg="$*"
+  # print to the terminal if we have one
+  test -t 1 && echo "$(date "+%Y-%m-%d %H:%M:%S %Z") [$$] ""$msg"
+  # write to log file
+  echo "$(date "+%Y-%m-%d %H:%M:%S %Z") [$$] ""$msg" >> $LOG_FILE
+}
+
+# Start
+Log "Starting ZIL Switcher."
 
 for pid in $(pgrep -f ${ZILLOOP}.sh); do
   if [ $pid != $$ ]; then
-    if [ "$INTERACTIVE" == "1" ]; then
-      echo "[$(date)]: ${ZILLOOP}.sh process is already running with PID $pid"
-      screen -ls | grep "zil"
-    fi
+    Log "${ZILLOOP}.sh process is already running with PID $pid."
+    test -t 1 && screen -ls | grep "zil"
+    test -t 1 && Log "Watch the log with: tail -f $LOG_FILE (Ctrl-c to stop)."
     exit 1
   fi
 done
 
-
 screen -wipe 2>&1 >/dev/null
 
+Log "Entering ZIL Loop Screen"
 screen -dmS zil -t zil ${HOME}/zil-miner-switch/${ZILLOOP}.sh
 
--- src/zil-loop.sh
+++ src/zil-loop.sh
@@ -2,8 +2,23 @@
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/opt/ethos/bin:/opt/ethos/sbin"
 
+LOG_FILE="/tmp/zil-switch.log"
+
+# Define Log Function
+Log() {
+  msg="$*"
+  # print to the terminal if we have one
+  test -t 1 && echo "$(date "+%Y-%m-%d %H:%M:%S %Z") [$$] ""$msg"
+  # write to log file
+  echo "$(date "+%Y-%m-%d %H:%M:%S %Z") [$$] ""$msg" >> $LOG_FILE
+}
+
+# Start
+Log "Starting ZIL Switcher Loop."
+
 # Make sure EthOS Miner is allowed for now
-allow
+Log "Allowing ethOS standard miner."
+allow || Log "ERROR: allow"
 
 # Loop forever
 while :
@@ -13,33 +28,42 @@ do
 for pid in $(pgrep -f $(basename $0)); do
   status=$(ps ax | grep $pid | grep SCREEN)
   if [[ -z $status ]] && [[ $pid != $$ ]]; then
-    echo "[$(date)]: Process is already running with PID $pid"
+    Log "Process is already running with PID $pid."
+    sleep 120
     exit 1
   fi
 done
 
 # Wait for Zilliqa ZIL PoW Time
-perl ${HOME}/zil-miner-switch/zil-waitfor-pow.pl
+Log "Wait for ZIL PoW Time"
+perl ${HOME}/zil-miner-switch/zil-waitfor-pow.pl || \
+  Log "WARN: zil-waitfor-pow returned non-zero exit code."
 
 # Stopp EthOS Miner
-disallow
-minestop
+Log "Stopping ethOS default miner"
+disallow || Log "ERROR: disallow"
+minestop || Log "ERROR: minestop"
 
 # Wait a few seconds
 sleep 15
 
 # Read config
+Log "Reading ZIL Switcher Config"
 source ${HOME}/zil-miner-switch/mineropts.txt
 source ${HOME}/zil-miner-switch/minerpool.txt
 
 # Start PhoenixMiner for ZIL Mining for 5 Minutes
-/opt/miners/phoenixminer/PhoenixMiner -pool ${POOL} -wal ${WALLET} -worker ${HOSTNAME} -proto 2 -gsi 15 -log 1 -logfile /run/miner.output -rmode 0 -timeout 5 ${OPTS}
+Log "Starting ZIL PhoenixMiner"
+/opt/miners/phoenixminer/PhoenixMiner -pool ${POOL} -wal ${WALLET} -worker ${HOSTNAME} -proto 2 -gsi 15 -log 1 -logfile /run/miner.output -rmode 0 -timeout 5 ${OPTS} || \
+  Log "WARN: ZIL PhoenixMiner returned non-zero exit code."
+Log "ZIL PhoenixMiner ended."
 
 # Wait a few seconds
 sleep 5
 
 # Restart EthOS Miner
-allow
+Log "Allowing ethOS standard miner."
+allow || Log "ERROR: allow"
 
 # Repeat loop
 done
